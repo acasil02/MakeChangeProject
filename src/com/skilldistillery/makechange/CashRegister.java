00d222641c54bce256496e47d67ad29158bc97c6
Angel <angel1179008@gmail.com>
2022-03-18 21:49:53 -0600

    Made some variables and changes my if and else statements

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,37 +6,41 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+
+		// listed variables
+		int cent = 0;
+		int cent5 = 0;
+		int cent10 = 0;
+		int cent25 = 0;
+		int dollar = 0;
+		int dollar5 = 0;
+		int dollar10 = 0;
+		int dollar20 = 0;
 		double price;
 		double amountTendered;
+		int changeGivenBack = amountTendered - price;
 		Scanner input;
-		
+
 		// User Story #1
 		// The user is prompted asking for the price of the item.
 		System.out.println("Please enter the price of the item: ");
-			price = sc.nextInt();
-			double result = price;
+		price = sc.nextInt();
+		double result = price;
 
 		// User Story #2
-		// The user is then prompted asking how much money was tendered by the customer.
-			System.out.println("Tendered amount given: ");
-			amountTendered = sc.nextInt();
-			double tendered = amountTendered;
+		// The user is then prompted asking how much money was tendered by the customer. along with how much they need and if exact change then done
+		System.out.println("Amount given: ");
+		amountTendered = sc.nextInt();
+		double tendered = amountTendered;
 
-		// User Story #3
-		// Display an appropriate message if the customer provided too little money or
-		// the exact amount.
-			if (amountTendered < price)
-				System.out.println("NOT SUFFICIENT AMOUNT: ");
-			
-			
-			
-		// User Story #4
-		// If the amount tendered is more than the cost of the item, display the number
-		// of bills and coins that should be given to the customer.
-			if (amountTendered > price)
-				System.out.println("test");
+		if (amountTendered < price) {
+			System.out.println("NOT SUFFICIENT AMOUNT: ");
+		}	else if (amountTendered == price) {
+			System.out.println("Exact amount. Thank you for your purchase!");
+		}	else {
+			System.out.println("Your change is: ");
+		}
+		
 
 	}
 
