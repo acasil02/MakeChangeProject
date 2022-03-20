7a0e87482eb4e5739ddb7111e90100bee68c4b6c
Angel <angel1179008@gmail.com>
2022-03-19 23:48:48 -0600

    Made lots of changes to the program along with changing to different methods and worked out how to correct the mistake of not redisplaying a menu system after being prompt to exit. I have also filled out my readme file.

--- README.md
+++ README.md
@@ -5,14 +5,14 @@
 ## Overview
 
 Cash register simulator.
-TODO. FINISH WRITTING THIS!!!
+As a first Java project with Skill Distillery, i was tasked to code a cash register. The user will have a menu option that will show to make a transaction or a quit option. After the transaction the change is there is any will be shown from greatest value to least.
 
 ## Technologies/Skills Used
 
 * Java
 * Eclipse
-* TODO: MORE
+* Git
 
 ## Lessons Learned
 
-TODO: WRITE THIS
+This project helped me read a lot more than simple labs we have done. I had to go over several times and re-test to see why certain values were given the wrong input and output. The more challenging part was to understand what variables needed to be establish and how to establish methods so it would work together.
--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -17,20 +17,23 @@ public class CashRegister {
 		int dollar10 = 0;
 		int dollar20 = 0;
 
-		boolean dontKeepAppRunning = displayMenu(sc);
+		boolean keepAppRunning = menuChoice(sc);
 
-		if (!dontKeepAppRunning) {
+		while (keepAppRunning) {
+			// menuChoice(sc);
 
 			// User Story #1
 			// The user is prompted asking for the price of the item.
 			System.out.println("Please enter the price of the item: ");
 			int price = convertForRoundingError(sc.nextDouble());
+			sc.nextLine();
 
 			// User Story #2
 			// The user is then prompted asking how much money was tendered by the customer.
 			// along with how much they need and if exact change then done
 			System.out.println("Amount given: ");
 			int amountTendered = convertForRoundingError(sc.nextDouble());
+			sc.nextLine();
 			int changeGivenBack = (amountTendered - price);
 			int changeGivenBackCopy = changeGivenBack;
 
@@ -66,12 +69,20 @@ public class CashRegister {
 						changeGivenBack -= 1;
 					}
 				}
-				System.out.println(changeGivenBackCopy + " pennies " + cent + " nickles " + cent5 + " dimes " + cent10
-						+ " quarters " + cent25 + " dollars " + dollar + " fives " + dollar5 + " tens " + dollar10
-						+ " twenties " + dollar20);
-			}
-		}
+//				System.out.println(changeGivenBackCopy + " pennies " + cent + " nickles " + cent5 + " dimes " + cent10
+//						+ " quarters " + cent25 + " dollars " + dollar + " fives " + dollar5 + " tens " + dollar10
+//						+ " twenties " + dollar20);
 
+				displayChangeGivenBack(changeGivenBackCopy, cent, cent5, cent10, cent25, dollar, dollar5, dollar10,
+						dollar20);
+				System.out.println(changeGivenBack);
+				System.out.println(changeGivenBackCopy);
+				changeGivenBack = 0;
+				changeGivenBackCopy = 0;
+			}
+			boolean decision = askForAnotherSale(sc);
+			keepAppRunning = decision;
+		}
 	}
 
 	public static int convertForRoundingError(double cash) {
@@ -82,34 +93,122 @@ public class CashRegister {
 
 	}
 
-	public static boolean displayMenu(Scanner sc) {
+	public static void displayMenu() {
 		System.out.println("---------------");
 		System.out.println("|    Menu     |");
 		System.out.println("| 1: Sale     |");
 		System.out.println("| 2: Logout   |");
 		System.out.println("---------------");
+	}
 
+	public static boolean menuChoice(Scanner sc) {
 		boolean keepDisplaying = true;
 		while (keepDisplaying) {
+			displayMenu();
 			int selection = sc.nextInt();
 			switch (selection) {
 			case 1:
 				System.out.println("Hello, Please begin sale!");
-				return false;
+				keepDisplaying = false;
+				return true;
 			case 2:
 				System.out.println("Shutting down system, goodbye!");
-				return true;
+				keepDisplaying = false;
+				return false;
 			default:
 				System.out.println("Please enter a valid option. Select 1 or 2");
 				break;
 			}
 		}
 		return true;
+	}
+
+	public static boolean askForAnotherSale(Scanner sc) {
+		boolean keepSelling = true;
+		System.out.println("Would you like to make another sale? Yes or No");
+		sc.nextLine();
+		String choice = sc.nextLine();
+		if (choice.equals("Yes") || choice.equals("yes") || choice.equals("Y") || choice.equals("y")) {
+			keepSelling = true;
+		} else if (choice.equals("No") || choice.equals("no") || choice.equals("N") || choice.equals("n")) {
+			keepSelling = false;
+
+		}
+		return keepSelling;
 
 	}
 
-	public static void displayChangeGivenBack() {
-
+	public static void displayChangeGivenBack(int changeGivenBackCopy, int cent, int cent5, int cent10, int cent25,
+			int dollar, int dollar5, int dollar10, int dollar20) {
+		System.out.println("Your change is: ");
+		while (changeGivenBackCopy > 0) {
+			if (dollar20 > 1) {
+				System.out.println(dollar20 + " twenty dollar bills");
+				changeGivenBackCopy -= (2000 * dollar20);
+				dollar20 = 0;
+			} else if (dollar20 == 1) {
+				System.out.println(dollar20 + " twenty dollar bill");
+				changeGivenBackCopy -= (2000 * dollar20);
+				dollar20 = 0;
+			} else if (dollar10 > 1) {
+				System.out.println(dollar10 + " ten dollar bills");
+				changeGivenBackCopy -= (1000 * dollar10);
+				dollar10 = 0;
+			} else if (dollar10 == 1) {
+				System.out.println(dollar10 + " ten dollar bill");
+				changeGivenBackCopy -= (1000 * dollar10);
+				dollar10 = 0;
+			} else if (dollar5 > 1) {
+				System.out.println(dollar5 + " five dollar bills");
+				changeGivenBackCopy -= (500 * dollar5);
+				dollar5 = 0;
+			} else if (dollar5 == 1) {
+				System.out.println(dollar5 + " five dollar bill");
+				changeGivenBackCopy -= (500 * dollar5);
+				dollar5 = 0;
+			} else if (dollar > 1) {
+				System.out.println(dollar + " one dollar bills");
+				changeGivenBackCopy -= (100 * dollar);
+				dollar = 0;
+			} else if (dollar == 1) {
+				System.out.println(dollar + " one dollar bill");
+				changeGivenBackCopy -= (100 * dollar);
+				dollar = 0;
+			} else if (cent25 > 1) {
+				System.out.println(cent25 + " quarters");
+				changeGivenBackCopy -= (25 * cent25);
+				cent25 = 0;
+			} else if (cent25 == 1) {
+				System.out.println(cent25 + " quarter");
+				changeGivenBackCopy -= (25 * cent25);
+				cent25 = 0;
+			} else if (cent10 > 1) {
+				System.out.println(cent10 + " dimes");
+				changeGivenBackCopy -= (10 * cent10);
+				cent10 = 0;
+			} else if (cent10 == 1) {
+				System.out.println(cent10 + " dime");
+				changeGivenBackCopy -= (10 * cent10);
+				cent10 = 0;
+			} else if (cent5 > 1) {
+				System.out.println(cent5 + " nickels");
+				changeGivenBackCopy -= (5 * cent5);
+				cent5 = 0;
+			} else if (cent5 == 1) {
+				System.out.println(cent5 + " nickel");
+				changeGivenBackCopy -= (5 * cent5);
+				cent5 = 0;
+			} else if (cent > 1) {
+				System.out.println(cent + " cents");
+				changeGivenBackCopy -= (1 * cent);
+				System.out.println(changeGivenBackCopy);
+				cent = 0;
+			} else if (cent == 1) {
+				System.out.println(cent + " cent");
+				changeGivenBackCopy -= (1 * cent);
+				System.out.println(changeGivenBackCopy);
+				cent = 0;
+			}
+		}
 	}
-
 }
